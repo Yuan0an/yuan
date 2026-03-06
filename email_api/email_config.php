be81d9aaa3126946fa2acfe0abbbfc81174938af
Yuan0an <calmayuanandrei@gmail.com>
2026-03-06 23:04:16 +0800

    Update SMTP configuration with valid credentials

--- email_api/email_config.php
+++ email_api/email_config.php
@@ -12,12 +12,12 @@ function getMailer() {
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'yourgmail@gmail.com';   // admin gmail
-    $mail->Password   = 'your_app_password';     // Gmail App Password
+    $mail->Username   = 'calmayuan0@gmail.com';   // admin gmail
+    $mail->Password   = 'lpxi ugef jwjh fcqk';     // Gmail App Password
     $mail->SMTPSecure = 'tls';
     $mail->Port       = 587;
 
-    $mail->setFrom('yourgmail@gmail.com', 'Resort Booking');
+    $mail->setFrom('calmayuan0@gmail.com', 'CK Resort');
 
     return $mail;
 }
